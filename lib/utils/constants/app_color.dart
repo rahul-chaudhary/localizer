3606e14c33bdc50f4c2e582babfa967776f0a941
Rahul Chaudhary <bossrc64@outlook.com>
2025-01-25 00:03:34 +0530

    ADDED: New color

--- lib/utils/constants/app_color.dart
+++ lib/utils/constants/app_color.dart
@@ -6,4 +6,5 @@ class AppColor {
   static const Color scaffoldColorLight = Color(0xfff4f2ec);
   static const Color primaryColor = Color(0xffc96442);
   static const Color textColorLight = Color(0xffffffff);
+  static const Color lightGrey = Color(0xffbebda6);
 }
