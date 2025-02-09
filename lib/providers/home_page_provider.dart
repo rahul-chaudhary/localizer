f19dba022e6fc908bf756b99f0c79b466c2177ab
Rahul Chaudhary <bossrc64@outlook.com>
2025-02-09 15:39:47 +0530

    ADDED: isKeyAlreadyPresent and more

--- lib/providers/home_page_provider.dart
+++ lib/providers/home_page_provider.dart
@@ -1,16 +1,27 @@
 import 'package:flutter/material.dart';
-
-import '../Models/TableRowItem.dart';
+import '../Models/table_row_item.dart';
+import '../Models/crud_tab.dart';
 
 class HomePageProvider extends ChangeNotifier {
-  final List<TableRowItem> _tableRowItems = [
-    TableRowItem(key: 'myKet', englishItem: 'YEAH', hindiItem: 'fsdf', marathiItem: 'dsfcd')
-  ];
+
+  CRUDTab _selectedTab = CRUDTab.create;
+  final List<TableRowItem> _tableRowItems = [];
+
+  CRUDTab get selectedTab => _selectedTab;
+
+  void setSelectedTab(CRUDTab tab) {
+    _selectedTab = tab;
+    notifyListeners();
+  }
 
   List<TableRowItem> get tableRowItems => _tableRowItems;
 
   void addTableRowItem(TableRowItem tableRowItem) {
     _tableRowItems.add(tableRowItem);
     notifyListeners();
+  }
+
+  bool isKeyAlreadyPresent(String key) {
+    return _tableRowItems.any((element) => element.key == key);
   }
 }
