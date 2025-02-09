10c77f1f64e2f4030dc43e533afb4006f4006585
Rahul Chaudhary <bossrc64@outlook.com>
2025-02-09 15:38:25 +0530

    ADDED: AppTextStyle

---
+++ lib/utils/constants/text_styles.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  AppTextStyles._();
+
+  static TextStyle titleTextStyle = const TextStyle(fontWeight: FontWeight.bold, fontSize: 22);
+
+}
