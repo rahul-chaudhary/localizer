950ea8c97755afc5ed1d85189dd7ebe8356d2f11
Rahul Chaudhary <bossrc64@outlook.com>
2025-02-09 15:36:07 +0530

    ADDED: Common style across whole app

---
+++ lib/utils/constants/app_styles.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+import 'app_color.dart';
+
+BoxDecoration get crudBoxDecoration => BoxDecoration(
+  borderRadius: BorderRadius.circular(10),
+  border: Border.all(color: AppColor.lightGrey),
+);
+
+EdgeInsets get containerPadding => const EdgeInsets.all(12.0);
