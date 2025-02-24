41b9527ec79b8614405d334d1c2672dec2357ca2
Rahul Chaudhary <bossrc64@outlook.com>
2025-02-24 12:58:57 +0530

    UPDATE: Formatting changes

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -7,10 +7,9 @@ import 'pages/home_page.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
