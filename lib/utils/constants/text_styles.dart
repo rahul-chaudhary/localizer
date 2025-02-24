712238d3256925264b24f5414cc95335264936b1
Rahul Chaudhary <bossrc64@outlook.com>
2025-02-24 13:00:35 +0530

    ADD: TextStyle

--- lib/utils/constants/text_styles.dart
+++ lib/utils/constants/text_styles.dart
@@ -4,5 +4,6 @@ class AppTextStyles {
   AppTextStyles._();
 
   static TextStyle titleTextStyle = const TextStyle(fontWeight: FontWeight.bold, fontSize: 22);
+  static TextStyle errorTextStyle = const TextStyle(color: Colors.red);
 
 }
