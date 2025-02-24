569e9a7a7861dc9ddda21ece123ce0285e898d99
Rahul Chaudhary <bossrc64@outlook.com>
2025-02-24 14:58:56 +0530

    ADDED: autocorrect property

--- lib/widgets/app_text_field.dart
+++ lib/widgets/app_text_field.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
-TextField myTextField({required String hintText, required TextEditingController controller}) {
+TextField myTextField({required String hintText, required TextEditingController controller, bool autocorrect = false}) {
   return TextField(
     controller: controller,
+    autocorrect: autocorrect,
     decoration: InputDecoration(
       hintText: hintText,
       border: const OutlineInputBorder(),
